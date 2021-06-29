3aad3db20b9332b76a0183728d3f92706f78b640
Gusarich <danielsedovzzz@gmail.com>
2021-06-29 20:56:42 +0300

    Tests are now cool

--- node/tests/lib.py
+++ node/tests/lib.py
@@ -4,22 +4,28 @@ import threading
 
 
 CMD_PREFIX = 'node ../src/main.js '
-
 process_index = 0
+status = []
+processes = []
 
 
-def _run_in_background(cmd, prefix=''):
-    process = subprocess.Popen(CMD_PREFIX + cmd,
+def _run_in_background(cmd, prefix, index, logging):
+    status[index] = True
+    process = subprocess.Popen('exec ' + CMD_PREFIX + cmd,
                                shell=True,
                                stdout=subprocess.PIPE,
                                stderr=subprocess.STDOUT)
+    processes.append(process)
     for line in process.stdout:
-        sys.stdout.write(prefix + line.decode())
+        if logging:
+            sys.stdout.write(prefix + line.decode())
+    status[index] = False
 
 
-def run_in_background(cmd):
+def run_in_background(cmd, logging):
     global process_index
     process_color = process_index % 8 + 1
     process_index += 1
     prefix = f'\x1b[1;{29 + process_color};{48 - process_color}m[{process_index}]\x1b[0m '
-    threading.Thread(target=_run_in_background, args=(cmd, prefix,)).start()
+    status.append(False)
+    threading.Thread(target=_run_in_background, args=(cmd, prefix, process_index - 1, logging), daemon=True).start()
---
+++ node/tests/test.py
@@ -0,0 +1,47 @@
+import lib
+from time import sleep
+
+
+COLORS = {
+    'header': '\033[95m',
+    'description': '\033[90m',
+    'passed': '\033[93m',
+    'failed': '\033[91m',
+    'clear': '\x1b[0m'
+}
+LOGGING = True
+
+total_tests = 1
+passed_tests = 0
+
+
+# TEST 1
+print(COLORS['header'] + 'Test #1')
+print(COLORS['description'] + 'Start several nodes' + COLORS['clear'])
+
+lib.run_in_background('--ws-port 11100 --genesis', logging=LOGGING)
+sleep(0.15)
+lib.run_in_background('--ws-port 11101 --peer ws://127.0.0.1:11100', logging=LOGGING)
+sleep(0.15)
+lib.run_in_background('--ws-port 11102 --peer ws://127.0.0.1:11100', logging=LOGGING)
+sleep(0.15)
+lib.run_in_background('--ws-port 11103 --http-port 8080 --peer ws://127.0.0.1:11101', logging=LOGGING)
+sleep(0.15)
+lib.run_in_background('--ws-port 11104 --peer ws://127.0.0.1:11102', logging=LOGGING)
+sleep(0.15)
+lib.run_in_background('--ws-port 11105 --http-port 8081 --peer ws://127.0.0.1:11104', logging=LOGGING)
+sleep(0.15)
+lib.run_in_background('--ws-port 11106 --peer ws://127.0.0.1:11104', logging=LOGGING)
+sleep(3)
+
+if all(lib.status):
+    passed_tests += 1
+    print(COLORS['passed'] + 'Test passed ✔')
+else:
+    print(COLORS['failed'] + 'Test failed ✘')
+print(f'Total {passed_tests}/{total_tests}' + COLORS['clear'])
+
+
+for process in lib.processes:
+    process.kill()
+exit()
