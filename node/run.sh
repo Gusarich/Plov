427de1cd61270a858e694f33f00b42dee1c42b99
Gusarich <danielsedovzzz@gmail.com>
2021-10-13 05:51:47 +0300

    node fix for sudo user

--- node/run.sh
+++ node/run.sh
@@ -1,1 +1,1 @@
-node node/main.js --ws-port 11100 --http-port 80 --genesis --keypair ~/.plov/keypair1
+"$(which node)" node/main.js --ws-port 11100 --http-port 80 --genesis --keypair ~/.plov/keypair1
