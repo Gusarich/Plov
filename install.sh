d247be519785312aa6a80d6758d7f8b7bf388dcf
Gusarich <danielsedovzzz@gmail.com>
2021-10-13 05:32:37 +0300

    update in installation script

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
-sudo apt update
+echo "
+" | sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install --lts
-sudo apt install python3.9
+echo "yes
+" | sudo apt-get install python3.9
 
-python3.9 -m pip install requests colorama
+python3 -m pip install requests colorama
 npm install bignumber.js tweetnacl tweetnacl-util express body-parser ws sync-fetch
 
 git clone https://github.com/Gusarich/Plov.git
