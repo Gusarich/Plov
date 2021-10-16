5a3860d43125c7d8f0ad4c038ef14802e31a2cb1
BUYT-1 <59949541+BUYT-1@users.noreply.github.com>
2021-10-16 23:44:02 +0300

    Remove input redirection in install script and format curl command

--- install.sh
+++ install.sh
@@ -1,16 +1,13 @@
-echo "
-" | sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get update
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+curl -o - https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install --lts
-echo "y
-" | sudo apt-get install python3.9
-echo "y
-" | sudo sudo apt install python3-pip
+sudo apt-get install -y python3.9
+sudo apt install -y python3-pip
 python3.9 -m pip install requests colorama
 
 git clone https://github.com/Gusarich/Plov.git
